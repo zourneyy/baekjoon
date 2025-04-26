19907c31430d4bc16bd66fa883cf2d822996defd
zourneyy <24ghkdwl@sookmyung.ac.kr>
2025-04-26 14:13:26 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,3 +1,7 @@
-word = input()
-word = word.swapcase()
-print(word)
+word = str(input())
+
+for i in word:
+    if i.isupper() == True:
+        print(i.lower(), end="")
+    else:
+        print(i.upper(), end="")
