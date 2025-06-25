350f85b55c44aab4c28a8b357a67aa816724910f
zourneyy <24ghkdwl@sookmyung.ac.kr>
2025-06-25 16:51:20 +0900

    [Bronze IV] Title: 특별한 날, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,19 @@
+month = int(input())
+day = int(input())
+
+if month == 2 and day == 18:
+    print("Special")
+
+elif month > 2:
+    print("After")
+
+elif month == 2 and day > 18:
+    print("After")
+
+elif month == 1:
+    print("Before")
+
+elif month == 2 and day < 18:
+    print("Before")
+
+
